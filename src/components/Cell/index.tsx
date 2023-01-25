17885aebe52eb7263028b70ba5187385215bcb2b
Arthur Pereira da Silva <arthur.pereira@academico.ifpb.edu.br>
2023-01-25 19:45:31 -0300

    fix: trocando o input por dropdown

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react';
-import { Input, TableCell } from 'semantic-ui-react';
+import { Dropdown, TableCell } from 'semantic-ui-react';
 import { AuthContext } from '../../contexts/auth';
 import api from '../../services/api';
 
@@ -10,15 +10,33 @@ type Props = {
 
 export default function Cell({ value, id }: Props) {
   const appContext = useContext(AuthContext);
-  const [name, setName] = useState(value);
-  const onUpdate = async function loadingCells() {
+  const [name, setName] = useState('');
+  const gymOptions = [
+    { key: 'pt', value: 'Peito', text: 'Peito' },
+    { key: 'ct', value: 'Costas', text: 'Costas' },
+    { key: 'ob', value: 'Ombro', text: 'Ombro' },
+    { key: 'tpz', value: 'Trapézio', text: 'Trapézio' },
+    { key: 'bc', value: 'Bíceps', text: 'Bíceps' },
+    { key: 'tc', value: 'Tríceps', text: 'Tríceps' },
+    { key: 'ay', value: 'Antebraço', text: 'Antebraço' },
+    { key: 'ptt', value: 'Posteior', text: 'Posteior' },
+    { key: 'gt', value: 'Glúteo', text: 'Glúteo' },
+    { key: 'ptr', value: 'Panturrilha', text: 'Panturrilha' },
+    { key: 'prn', value: 'Perna (completa)', text: 'Perna (completa)' },
+    { key: 'bcc', value: 'Braço (completo)', text: 'Braço (completo)' },
+    { key: 'abd', value: 'Abdômen', text: 'Abdômen' },
+    { key: 'lbr', value: 'Lombar', text: 'Lombar' },
+  ];
+
+  const onUpdate = async function (value: string) {
+    console.log(value);
     api.defaults.headers.common['Authorization'] = `Bearer ${appContext?.token}`;
-    await api.put(`/update-table/${id}`, { name });
+    await api.put(`/update-table/${id}`, { name: value }).then(() => alert('Foi'));
   };
 
   return (
     <TableCell key={id}>
-      <Input value={name} onChange={(e) => setName(e.target.value)} onBlur={onUpdate} />
+      <Dropdown placeholder="-" fluid selection options={gymOptions} defaultValue={value} onChange={(e, value) => onUpdate(value.value as string)} />
     </TableCell>
   );
 }
